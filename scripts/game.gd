ca7ab40501488aea26a5d2963c6c8e18d756771f
Milan Gruner <milangruner@gmail.com>
2024-05-16 19:41:00 +0200

    Setup main game scene

---
+++ scripts/game.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+var current_year = 2015
+
+@onready var button: Button = %Button
+@onready var year_label: Label = %YearLabel
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
+
+func _on_next_year_button_pressed() -> void:
+	current_year += 1
+	year_label.text = str(current_year)
