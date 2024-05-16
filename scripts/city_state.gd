8384986bddaee4f8b9c726b280e6ab4b1c7c250a
Amanda Eames <amandaeames@Amandas-MacBook-Pro.local>
2024-05-16 16:37:43 -0300

    citystate

--- scripts/city_state.gd
+++ scripts/city_state.gd
@@ -1,1 +1,11 @@
 extends Node
+
+signal changed
+
+var previous_policies = []
+var current_year
+var budget = 0 
+var emissions_sector1 = 100
+var emissions_sector2 = 150 
+var emissions_sector3 = 200 
+var roi = 0 
