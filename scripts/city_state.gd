94f7dd4b9a06fdb9596da3b0eb98553aa8e4afb5
Milan Gruner <milangruner@gmail.com>
2024-05-16 22:51:16 +0200

    Adjust starting city state

--- scripts/city_state.gd
+++ scripts/city_state.gd
@@ -3,9 +3,9 @@ extends Node
 signal changed
 
 var previous_policies = []
-var current_year
-var budget = 0 
-var emissions_sector1 = 100
-var emissions_sector2 = 150 
-var emissions_sector3 = 200 
-var roi = 0 
+var current_year = 2015
+var budget = 10000000
+var emissions_sector1 = 100000
+var emissions_sector2 = 150000
+var emissions_sector3 = 200000
+var roi = 0
