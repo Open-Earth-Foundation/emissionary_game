8cc2025638b60866c4a3f584574e4a9966a69259
Milan Gruner <milangruner@gmail.com>
2024-05-16 22:44:45 +0200

    Choose 3 policies at random, fix opening decision making again breaking it

--- scripts/decision_making.gd
+++ scripts/decision_making.gd
@@ -1,23 +1,25 @@
 extends PopupPanel
 
 @onready var policyList = $VBoxContainer/PolicyList
-@onready var policyCard = $VBoxContainer/PolicyList/PolicyCard
+@onready var policyCard = $VBoxContainer/PolicyCard
 
 func newPolicies(year):
 	var policies = choose_policies(Policies.available)
-	var seenOnce = false
-	for policy in policies:
-		var newCard
-		if seenOnce:
-			newCard = policyCard.duplicate()
-			policyList.add_child(newCard)
-		else:
-			newCard = policyCard
-			seenOnce = true
-		newCard.get_node('PolicyTitle').text = policy['title']
+	var selected_policies = []
+	for i in 3:
+		selected_policies.push_back(policies.pick_random())
+
+	for previous_card in policyList.get_children():
+		previous_card.queue_free()
+
+	for policy in selected_policies:
+		var newCard = policyCard.duplicate()
+		policyList.add_child(newCard)
+		newCard.show()
+		newCard.get_node('PolicyTitle').text = policy['name']
 		newCard.get_node('PolicyDescription').text = policy['description']
 		newCard.get_node('PolicySectorContainer/PolicySector').text = policy['sector']
-		newCard.get_node('PolicyImpactContainer/PolicyImpact').text = str(policy['impact']) + "t CO2eq"
+		newCard.get_node('PolicyImpactContainer/PolicyImpact').text = str(policy['emission reduction']) + "t CO2eq"
 		newCard.get_node('PolicyPropertiesContainer/PolicyCostContainer/PolicyCost').text = "$" + str(policy['cost'])
 		newCard.get_node('PolicyPropertiesContainer/PolicyROIContainer/PolicyROI').text = "$" + str(policy['roi'])
 		newCard.set_meta("policy", policy)
@@ -37,11 +39,11 @@ func choose_policies(ap):
 func apply_policy(policy: Dictionary) -> void:
 	match policy.sector:
 		"Energy":
-			CityState.emissions_sector1 -= policy.impact
+			CityState.emissions_sector1 -= int(policy["emission reduction"])
 		"Transport":
-			CityState.emissions_sector2 -= policy.impact
+			CityState.emissions_sector2 -= int(policy["emission reduction"])
 		"Waste":
-			CityState.emissions_sector3 -= policy.impact
-	CityState.roi += policy.roi
-	CityState.budget -= policy.cost
+			CityState.emissions_sector3 -= int(policy["emission reduction"])
+	CityState.roi += int(policy.roi)
+	CityState.budget -= int(policy.cost)
 	CityState.previous_policies.push_back(policy)
