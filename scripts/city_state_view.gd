8891dd2fd3ad095e46ed9a22f2731917de2fcd4e
Milan Gruner <milangruner@gmail.com>
2024-05-17 00:40:31 +0200

    Start emissions bars at 100%

--- scripts/city_state_view.gd
+++ scripts/city_state_view.gd
@@ -11,6 +11,6 @@ func _ready():
 	CityState.changed.connect(state_changed)
 
 func state_changed():
-	progress_bar.value = 100 * CityState.emissions_sector1 / float(max_emissions)
-	progress_bar_2.value = 100 * CityState.emissions_sector2 / float(max_emissions)
-	progress_bar_3.value = 100 * CityState.emissions_sector3 / float(max_emissions)
+	progress_bar.value = 100 * CityState.emissions_sector1 / float(CityState.INITIAL_EMISSIONS_SECTOR1)
+	progress_bar_2.value = 100 * CityState.emissions_sector2 / float(CityState.INITIAL_EMISSIONS_SECTOR2)
+	progress_bar_3.value = 100 * CityState.emissions_sector3 / float(CityState.INITIAL_EMISSIONS_SECTOR3)
